9160f71dadb60579749544c52d7f6a56e539796a
pmiljic <32903939+pmiljic@users.noreply.github.com>
2020-03-17 16:07:55 +0100

    Edit menu

--- includes/section-menu.php
+++ includes/section-menu.php
@@ -6,30 +6,22 @@ $logo = (carbon_get_theme_option('crb_menu_logo_photo'));
 </div>
 
 <div class="d-flex justify-content-end">
-    <nav class="navbar navbar-expand-xl navbar-light">
+    <nav class="navbar navbar-expand-xl navbar-light menu__navbar">
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto list">
-
-                <li class="nav-item dropdown list__item">
-                    <a class="dropdown-toggle menu__href" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        COLOCATION
-                    </a>
-                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa fa-angle-down"></i>
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDreopdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <?php
+            if (has_nav_menu('top-menu')) {
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'top-menu',
+                        'menu_class' => 'navbar-nav mr-auto list'
+                    )
+                );
+            }
+            ?>
             <button class="menu__btn-search">
                 <div class="menu__btn-search-div d-flex align-items-center justify-content-center">
                     <i class="fa fa-search"></i>
--- template-home.php
+++ template-home.php
@@ -8,7 +8,7 @@ Template Name: Home
 
 <section class="menu">
     <div class="container-fluid">
-        <div class="row justify-content-between align-items-center">
+        <div class="row menu__row justify-content-between">
             <?php get_template_part('includes/section', 'menu'); ?>
         </div>
     </div>
